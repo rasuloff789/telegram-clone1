6b4129eaf11c75260b91ada779773dc6815e89d6
rasuloff789 <rasuloff789@gmail.com>
2020-10-17 05:10:53 +0500

    rasul section end :)))

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+  $("#chat-item").click( () => {
+    $("#chat-item").toggleClass('bg-info');
+    $("#done").addClass('d-inline-block');
+    $("#done").toggleClass('text-white');
+    $("#done").toggleClass('text-success');
+    $("#time").toggleClass('text-white');
+    $("#time").toggleClass('text-secondary');
+    $("#chatname").toggleClass('text-white');
+    $("#mute").toggleClass('text-white');
+    $("#mute").toggleClass('text-secondary');
+    $("#chat-author").toggleClass('text-white');
+    $("#chat-author").toggleClass('text-dark');
+    $("#chat-text").toggleClass('text-white');
+    $("#chat-text").toggleClass('text-secondary');
+    $("#pushpin").toggleClass('text-white');
+    $("#pushpin").toggleClass('text-secondary');
+    $("#pushpin").toggleClass('border-white');
+    $("#pushpin").toggleClass('border-secondary');
+  })
+  $("p").dblclick(function(){
+    $("#pushpin").toggleClass('d-inline-block')
+  });
+});
+
+
+
+
+
